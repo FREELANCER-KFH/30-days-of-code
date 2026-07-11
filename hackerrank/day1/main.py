385b7c3cf0c7adb7e3a1d6a1443f1da1f8e5bcc5
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-07-11 16:16:28 +0000

    Agregando contenido de codigo y documentacion a la carpeta "Day1"

---
+++ hackerrank/day1/main.py
@@ -0,0 +1,24 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+entero = 0
+decimal = 0.0
+cadena_texto = ''
+
+# Read and save an integer, double, and String to your variables.
+entero = int(input('Ingrese un numero entero: '))
+decimal = float(input('Ingrese un numero decimal: '))
+cadena_texto = input('Ingrese un texto: ')
+
+# Print the sum of both integer variables on a new line.
+print(f'La suma entre los numero enteros es: {i+entero}')
+
+# Print the sum of the double variables on a new line.
+print(f'La suma entre los numero decimales es: {d+decimal}')
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(f'La concatenacion entre los textos es: {s}{cadena_texto}')
+
+print('muchas gracias por usar este programa, bye!!!')
